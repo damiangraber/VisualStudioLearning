8e31d3ce0933a2c3d177cff8f555e60235c1b8f0
Damian Grabowski <damian.grabowski7@gmail.com>
2019-01-30 10:45:11 +1100

    first commit

---
+++ WebApplicationMsDocsBeginner/WebApplicationMsDocsBeginner/Pages/Create.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationMsDocsBeginner.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public CreateModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync();
+            Message = $"Customer {Customer.Name} added!";
+            return RedirectToPage("/Index");
+        }
+    }
+}
